5088ae2d938712b6e8bc854075e6a85633afa137
uridr <orioldomingoroig@gmail.com>
2020-05-27 23:11:57 +0200

    custom db

--- data/wiki_data/monolingual/filter.py
+++ data/wiki_data/monolingual/filter.py
@@ -5,11 +5,10 @@ import sys
 def main(argv):
 	filepath = argv[0]
 	min_characters = 80     # minimum characters within a paragraph
-	max_characters = 80   # maximum characters within a paragraph
+	max_characters = 80*1.5   # maximum characters within a paragraph
 	#Read line by line: Keep only targets
 	with open(filepath) as fp:
 		paragraph = fp.readline()
-		print(paragraph)
 		while paragraph:
 			length = len(paragraph)
 			if length > min_characters:  
