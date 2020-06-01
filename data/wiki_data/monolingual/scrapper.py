2810c91e02569045912c73a3925f3a247a048042
uridr <orioldomingoroig@gmail.com>
2020-06-01 19:31:03 +0200

    monolingual data available

--- data/wiki_data/monolingual/scrapper.py
+++ data/wiki_data/monolingual/scrapper.py
@@ -1,8 +1,17 @@
+import json
 import time
 import requests
+import closest_entities           
 from bs4 import BeautifulSoup
 
 filter_link = 'https://en.wikipedia.org/wiki/'
+JSON_PATH = "delex_dict.json"
+
+def readJSON():
+	with open(JSON_PATH) as json_file:
+		data = json.load(json_file)
+
+	return data
 
 
 def getWkDataIntro(soup):
@@ -27,17 +36,35 @@ def explore(entities):
 
 	for entity in entities:
 
-		url = filter_link + entity
-		soup = getSoup(url)
+		closest = closest_entities.closestEntities(entity)
+		closest.append(entity)
+		
+		for close_entity in closest:
+		
+			url = filter_link + close_entity
+			soup = getSoup(url)
 
-		if soup:
-			getWkDataIntro(soup)
+			if soup:
+				getWkDataIntro(soup)
 
 
 if __name__ == '__main__':
 	
 	start = time.time()
+	print('Loading Embeddings...')
+	closest_entities.load_embeddings()
 	print('Exploring...')
-	explore(['Barcelona', 'Madrid', 'Tokyo', 'Delhi', 'Mexico City', 'California', 'France', 'Germany', 'Lady Gaga', 'Bruno Mars','Emminem','Beethoven', 'Big Ben', 'Stanford', 'Google', 'Colosseum','Italy', 'George_W._Bush', 'Barack Obama','Merkel','ESADE_Business_School','Eiffel_Tower','Lionel Messi', 'Batman','Spyderman', 'Michael Phelps', 'Rafael Nadal','Francesco Totti','Audi R8','Pizza','Salad','Bread'])
+	#explore(['Barcelona', 'Madrid', 'Tokyo', 'Delhi', 'Mexico City', 'California', 'France', 'Germany', 'Lady Gaga', 'Bruno Mars','Emminem','Beethoven', 'Big Ben', 'Stanford', 'Google', 'Colosseum','Italy', 'Barack Obama','Mariano Rajoy','ESADE_Business_School','Ronaldinho','Usain Bolt','Lionel Messi','Tiger Woods' ,'Batman','Spyderman', 'Michael Phelps', 'Rafael Nadal','Francesco Totti','Audi R8','Seat Ibiza','Pizza','Salad','Bread','Potatoes','Rice','Fideua','Range Rover Velar','French fries','Hamburger','Adele','Ozuna','Eiffel Tower','Golden Gate'])
+	data = readJSON()
+	global_entities = []
+	for key in data:
+		entities = []
+		for entity in data[key]:
+			word = ' '.join(entity.split('_'))
+			if not word in global_entities: 
+				entities.append(word)
+				global_entities.append(word)
+		explore(entities)
 	finish = time.time()
 	print(finish-start)
+	print(len(global_entities))
