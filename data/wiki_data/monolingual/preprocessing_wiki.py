19ed50ad14cee048ea63f131faa026c95416b40f
RoserCantenysSaba <r.cantenys@gmail.com>
2020-06-02 12:44:44 +0200

    script modified () 1st, [] later

--- data/wiki_data/monolingual/preprocessing_wiki.py
+++ data/wiki_data/monolingual/preprocessing_wiki.py
@@ -5,6 +5,7 @@ with open(sys.argv[1], 'r') as in_:
 	lines = in_.readlines()
 
 for line in lines:
-	line = re.sub("[\(\[].*?[\)\]]", "", line)
+	line = re.sub("[\(].*?[\)]", "", line)
+	ine = re.sub("[\[].*?[\]]", "", line)
 
 	print(line, file = open(sys.argv[2], 'a'))
