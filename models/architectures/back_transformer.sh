76b40ea252f1a35060a868472d163aa570b89670
berges99 <daviid.bergees@gmail.com>
2020-06-18 17:45:59 +0200

    Uploaded latest script versions

--- models/architectures/back_transformer.sh
+++ models/architectures/back_transformer.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 
+while [[ "$#" -gt 0 ]]; do
+  case "$1" in
+    # Parse configuration file path
+    -c) CONFIG="$2"; shift 2;;
+    # Parse data path
+    -f) FORMAT="$2"; shift 2;;
+    # Parse pretrained embeddings path
+    -e) EMB="$2"; shift 2;;
+    # Parse pretrained embeddings dimension
+    -d) DIM="$2"; shift 2;;
+    # Handle unknown parameters
+    -*|--*|*) echo "Unknown parameter: $1"; exit 2;;
+    # Store positional arguments
+    #*) POSITIONAL+=("$1"); shift 1;;
+  esac
+done
+
+
 # Global parameters
 ARCHITECTURE=back_transformer
-CONFIG_FILES=$1
-DATA_VERSION=$2
 
 # Source config file variables to current bash session
-. "$CONFIG_FILES"
+. "$CONFIG"
 
 CHECKPOINTS="checkpoints/$ARCHITECTURE"
 
@@ -38,49 +54,71 @@ writeVersionConfig ()
     echo "Embedding dimension: $6" >> $VERSION/config.txt
     echo "FFN dimension: $7" >> $VERSION/config.txt
     echo "Attention heads: $8" >> $VERSION/config.txt
-}
-
-writeVersionConfig ()
-{
-    # Create text file and write all the configuration parameters
-    echo "Training epochs: $1" >> $VERSION/config.txt
-    echo "Batch size: $2" >> $VERSION/config.txt
-    echo "Activation function: $3" >> $VERSION/config.txt
-    echo "Dropout: $4" >> $VERSION/config.txt
-    echo "Number of encoder/decoder layers: $5" >> $VERSION/config.txt
-    echo "Embedding dimension: $6" >> $VERSION/config.txt
-    echo "FFN dimension: $7" >> $VERSION/config.txt
-    echo "Attention heads: $8" >> $VERSION/config.txt
+    echo "Learned encoder position embeddings: $9" >> $VERSION/config.txt
+    echo "Learned decoder position embeddings: ${10}" >> $VERSION/config.txt
+    echo "Use cross + self-attention: ${11}" >> $VERSION/config.txt
 }
 
 
 # Train the network
 for epoch in "${EPOCHS[@]}"; do
-    for b in "${BATCH_SIZE[@]}"; do
-        for act in "${ACTIVATION[@]}"; do
-            for drop in "${DROPOUT[@]}"; do
-                for layers in "${LAYERS[@]}"; do
-                    for emb_dim in "${EMB_DIM[@]}"; do
-                        for ffn_dim in "${FFN_DIM[@]}"; do
-                            for heads in "${ATT_HEADS[@]}"; do
-                                # Determine version and build directories
-                                determineVersion
-                                # Write config version into .txt file
-                                writeVersionConfig "$epoch" "$b" "$act" "$drop" "$layers" "$emb_dim" "$ffn_dim" "$heads"
-                                # Train the model
-                                fairseq-train $DATA_VERSION \
-                                              --task translation -s lex -t triple \
-                                              --criterion label_smoothed_cross_entropy --label-smoothing 0.1 \
-                                              --optimizer adam --adam-betas '(0.9, 0.98)' --clip-norm 0.0 \
-                                              --lr 1e-03 --lr-scheduler inverse_sqrt --warmup-updates 4000 --warmup-init-lr 1e-07 \
-                                              --max-epoch $epoch --batch-size $b \
-                                              --arch transformer_iwslt_de_en --share-all-embeddings \
-                                              --activation-fn $act --dropout $drop \
-                                              --encoder-layers $layers --decoder-layers $layers \
-                                              --encoder-embed-dim $emb_dim --decoder-embed-dim $emb_dim \
-                                              --encoder-ffn-embed-dim $ffn_dim --decoder-ffn-embed-dim $ffn_dim \
-                                              --encoder-embed-path ../glove.6B.200d.txt --decoder-embed-path ../glove.6B.200d.txt \
-                                              --encoder-attention-heads $heads --decoder-attention-heads $heads \
-                                              --save-dir $SUB_CHE --no-epoch-checkpoints \
-                                              --tensorboard-logdir $SUB_LOG --log-format json
-done; done; done; done; done; done; done; done
+  for b in "${BATCH_SIZE[@]}"; do
+    for act in "${ACTIVATION[@]}"; do
+      for drop in "${DROPOUT[@]}"; do
+        for layers in "${LAYERS[@]}"; do
+          for emb_dim in "${EMB_DIM[@]}"; do
+            for ffn_dim in "${FFN_DIM[@]}"; do
+              for heads in "${ATT_HEADS[@]}"; do
+                for enc_pos in "${ENC_POS[@]}"; do
+                  for dec_pos in "${DEC_POS[@]}"; do
+                    for cross in "${CROSS[@]}"; do
+                      # Determine version and build directories
+                      determineVersion
+                      # Write config version into .txt file
+                      writeVersionConfig "$epoch" "$b" "$act" "$drop" "$layers" "$emb_dim" \
+                                         "$ffn_dim" "$heads" "$enc_pos" "$dec_pos" "$cross"
+
+                      if [ ! -z "$EMB" ]; then
+                        pretrained="--encoder-embed-path $EMB --decoder-embed-path $EMB"
+                        # Determine embedding dimension
+                        use_emb_dim="$DIM"
+                      else
+                        pretrained=""
+                        use_emb_dim="$emb_dim"
+                      fi
+
+                      # Determine learned position embeddings flags
+                      if [[ "$enc_pos" == "True" && "$dec_pos" == "True" ]]; then
+                        learned_pos="--encoder-learned-pos --decoder-learned-pos"
+                      elif [ "$enc_pos" == "True" ]; then
+                        learned_pos="--encoder-learned-pos"
+                      elif [ "$dec_pos" == "True" ]; then
+                        learned_pos="--decoder-learned-pos"
+                      else
+                        learned_pos=""
+                      fi
+
+                      # Perform cross+self-attention
+                      if [ "$cross" == "True" ]; then
+                        cross="--cross-self-attention"
+                      else
+                        cross=""
+                      fi
+
+                      # Train the model
+                      fairseq-train $FORMAT \
+                                    --task translation -s lex -t triple \
+                                    --criterion label_smoothed_cross_entropy --label-smoothing 0.1 \
+                                    --optimizer adam --adam-betas '(0.9, 0.98)' --clip-norm 0.0 \
+                                    --lr 1e-03 --lr-scheduler inverse_sqrt --warmup-updates 4000 --warmup-init-lr 1e-07 \
+                                    --max-epoch $epoch --batch-size $b \
+                                    --arch transformer_iwslt_de_en --share-all-embeddings $learned_pos $pretrained \
+                                    --activation-fn $act --dropout $drop $cross \
+                                    --encoder-layers $layers --decoder-layers $layers \
+                                    --encoder-embed-dim $use_emb_dim --decoder-embed-dim $use_emb_dim \
+                                    --encoder-ffn-embed-dim $ffn_dim --decoder-ffn-embed-dim $ffn_dim \
+                                    --encoder-attention-heads $heads --decoder-attention-heads $heads \
+                                    --save-dir $SUB_CHE --no-epoch-checkpoints \
+                                    --tensorboard-logdir $SUB_LOG --log-format json
+
+done; done; done; done; done; done; done; done; done; done; done
--- models/run_model.sh
+++ models/run_model.sh
@@ -8,99 +8,81 @@
 #SBATCH --job-name=fairseq
 #SBATCH --output=logs/fairseq_%j.log
 
-# Parse architecture command line options
-ARCH=${1:-"transformer"}
+# POSITIONAL=()
+while [[ "$#" -gt 0 ]]; do
+	case "$1" in
+		# Parse the architecture to be used
+		-a|--architecture) ARCH="$2"; shift 2;;
+		-a=*|--architecture=*) ARCH="${1#*=}"; shift 1;;
+		# Parse the configuration file to be used
+		-c|--config-file) CONF="$2"; shift 2;;
+		-c=*|--config-file=*) CONF="${1#*=}"; shift 1;;
+		
+		# Parse format to be used
+		-f|--format) FORMAT="$2"; shift 2;;
+		-f=*|--format=*) FORMAT="${1#*=}"; shift 1;;
+		# Parse BPE number of words
+		-b|--bpe) BPE="$2"; shift 2;;
+		-b=*|--bpe=*) BPE="${1#*=}"; shift 1;;
+
+		# Add the possiblity to use direct relative path to data
+		-p|--data-path) DATAPATH="$2"; shift 2;;
+		-p=*|--data-path=*) DATAPATH="$2"; shift 1;;
+
+		# Parse embeddings source
+		-s|--emb-source) SOURCE="$2"; shift 2;;
+		-s=*|--emb-source=*) SOURCE="${1#*=}"; shift 1;;
+		# Parse embedding dimension
+		-d|--emb-dimension) DIMENSION="$2"; shift 2;;
+		-d=*|--emb-dimension=*) DIMENSION="${1#*=}"; shift 1;;
+		
+		# Handle unknown parameters
+		-*|--*|*) echo "Unknown parameter: $1"; exit 2;;
+		# Store positional arguments
+		# *) POSITIONAL+=("$1"); shift 1;;
+	esac
+done
+
 # Determine architecture
 ARCHITECTURE=architectures/$ARCH.sh
 # Check if model architecture is defined
 if [ ! -f "$ARCHITECTURE" ]; then
-	echo "Specified architecture does not exist!"
+	echo "Unknown architecture: $ARCH"
 	echo "Available architectures are:"
 	ls architectures/
-	exit 9999
+	exit 2
 fi
 
-# Use the last config file as the default configuration
-N=($(ls config_files/$ARCH | wc -l))
-# Parse configuration file command line options
-CONF=${2:-$N}
 # Determine config file
-CONFIG_FILES=config_files/$ARCH/$CONF.config
-if [ ! -f "$CONFIG_FILES" ]; then
-	echo "'$CONFIG_FILES' configuration file does not exist!"
+CONFIG=config_files/$ARCH/$CONF.config
+if [ ! -f "$CONFIG" ]; then
+	echo "Unknown configuration file: $CONF"
 	echo "Available configuration files are:"
 	ls config_files/$ARCH/
-	exit 9999
+	exit 2
 fi
 
-# Parse data command line options
-FORMAT=${3:-"delex"}
-# Delexicalised data format options
-if [ "$FORMAT" == "delex" ]; then
-	FORMAT_VERSION=../format_delex
-	# Parse number of BPE words to be used
-	BPE=${4:-"1000"}
-	# Determine data version relative path
-	if [ "$BPE" == "500" ]; then
-		FORMAT_VERSION=$FORMAT_VERSION/BPE_500
-	elif [ "$BPE" == "1000" ]; then
-		FORMAT_VERSION=$FORMAT_VERSION/BPE_1_000
-	elif [ "$BPE" == "5000" ]; then
-		FORMAT_VERSION=$FORMAT_VERSION/BPE_5_000
-	elif [ "$BPE" == "7000" ]; then
-		FORMAT_VERSION=$FORMAT_VERSION/BPE_7_000
-	# Handle incorrect inputs
+# Determine data to be used
+if [ ! -z "$DATAPATH" ]; then
+	FORMAT="$DATAPATH"
+elif [ "$FORMAT" == "delex" ]; then
+	FORMAT="../format/$FORMAT"
+	# Use bite-pair-encoding
+	if [ ! -z "$BPE" ]; then
+		FORMAT+="/BPE_${BPE}"
 	else
-		echo "Number of BPE words not supported!"
-		exit 9999
-	fi
-# Lexicalised data format options
-elif [ "$FORMAT" == "lex" ]; then
-	FORMAT_VERSION=../format_lex
-	EMBEDDING_PATH=../embeddings
-	# Determine lexicalised data version to be used
-	if [ "$ARCH" == "back_transformer" ]; then
-		FORMAT_VERSION=$FORMAT_VERSION/LOW_CAMEL_FULL_BT_MONO
-	else
-		FORMAT_VERSION=$FORMAT_VERSION/LOW_CAMEL
-	fi
-	# Parse embedding options to be used
-	EMB_SOURCE=${4:-glove}
-	EMB_DIM=${5:-"300"}
-	# Determine embeddings to be used
-	if [ "$EMB_SOURCE" == "glove" ]; then
-		EMBEDDING_PATH=$EMBEDDING_PATH/glove
-		# Determine dimension
-		if [ "$EMB_DIM" == "50" ]; then
-			EMBEDDING_PATH=$EMBEDDING_PATH/glove.6B.50d.txt
-		elif [ "$EMB_DIM" == "100" ]; then
-			EMBEDDING_PATH=$EMBEDDING_PATH/glove.6B.100d.txt
-		elif [ "$EMB_DIM" == "200" ]; then
-			EMBEDDING_PATH=$EMBEDDING_PATH/glove.6B.200d.txt
-		elif [ "$EMB_DIM" == "300" ]; then
-			EMBEDDING_PATH=$EMBEDDING_PATH/glove.6B.300d.txt
-		else
-			echo "Embedding dimension not supported for $EMB_SOURCE embeddings!"
-			exit 9999
-		fi
-	elif [ "$EMB_SOURCE" == "enwiki" ]; then
-		EMBEDDING_PATH=$EMBEDDING_PATH/enwiki
-		# Determine dimension
-		if [ "$EMB_DIM" == "100" ]; then
-			EMBEDDING_PATH=$EMBEDDING_PATH/enwiki_20180420_100d.txt
-		elif [ "$EMB_DIM" == "300" ]; then
-			EMBEDDING_PATH=$EMBEDDING_PATH/enwiki_20180420_300d.txt
-		else
-			echo "Embedding dimension not supported for $EMB_SOURCE embeddings!"
-			exit 9999
-		fi
-	else
-		echo "Embeddings source is not supported!"
-		exit 9999
+		FORMAT+="/LOW_CAMEL"
 	fi
 else
-	echo "Specified data version is not supported!"
-	exit 9999
+	FORMAT+="/LOW_CAMEL"
+	if [ "$ARCH" == "back_transformer" ]; then
+		FORMAT+="_FULL_BT_MONO"
+	fi
+fi
+# Check if data path exists
+if [ ! -d "$FORMAT" ]; then
+	echo "Unknown data format: $FORMAT"
+	exit 2
 fi
 
 # Determine checkpoints directory
@@ -110,10 +92,27 @@ if [ ! -d "$CHECKPOINTS" ]; then
 	mkdir $CHECKPOINTS
 fi
 
-# Verbose some useful info
-echo "Training model: $ARCHITECTURE with config file: $CONFIG_FILES and data version: $FORMAT_VERSION"
-if [ ! -z "$EMBEDDING_PATH" ]; then
-	echo "Using pretrained embeddings: $EMBEDDING_PATH"
-fi
+# Output some useful information
+echo "Training model: $ARCHITECTURE"
+echo "Configuration file: $CONFIG"
+echo "Data: $FORMAT"
 
-bash $ARCHITECTURE $CONFIG_FILES $FORMAT_VERSION $EMBEDDING_PATH
+# Train with pretrained embeddings
+if [ ! -z "$SOURCE" ]; then
+	EMB="../embeddings/$SOURCE"
+	if [ "$SOURCE" == "glove" ]; then
+		EMB="$EMB/glove.6B.${DIMENSION}d.txt"
+	else
+		EMB="$EMB/enwiki_20180420_${DIMENSION}d.txt"
+	fi
+	# Check if embeddings exist
+	if [ ! -f "$EMB" ]; then
+		echo "Unknown pretrained embeddings path: $EMB"
+		exit 2
+	fi
+	echo "Pretrained embeddings: $EMB"
+	sh $ARCHITECTURE -c $CONFIG -f $FORMAT -e $EMB -d $DIMENSION
+# Train without pretrained embeddings
+else
+	sh $ARCHITECTURE -c $CONFIG -f $FORMAT
+fi
