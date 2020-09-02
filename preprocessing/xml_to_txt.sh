7d3faaed6ffd5d5f816ef99f96e1783b1423b76c
uridr <orioldomingoroig@gmail.com>
2020-09-02 12:05:19 +0200

    command update

--- preprocessing/xml_to_txt.sh
+++ preprocessing/xml_to_txt.sh
@@ -1,14 +1,14 @@
 python3 webnlg_baseline_input.py -i ../data/benchmark/original/
 
-rm all*.lex ../data/datasets/preprocessed/all/
-rm all*.triple ../data/datasets/preprocessed/all/
+mv all*.lex ../data/datasets/preprocessed/all/
+mv all*.triple ../data/datasets/preprocessed/all/
 
-rm dev*.lex ../data/benchmark/preprocessed/dev/
-rm dev*.triple ../data/benchmark/preprocessed/dev/
+mv dev*.lex ../data/benchmark/preprocessed/dev/
+mv dev*.triple ../data/benchmark/preprocessed/dev/
 
 
 mv test*.lex ../data/benchmark/preprocessed/test/
 mv test*.triple ../data/benchmark/preprocessed/test/
 
-rm train*.lex ../data/benchmark/preprocessed/train/
-rm train*.triple ../data/benchmark/preprocessed/train/
+mv train*.lex ../data/benchmark/preprocessed/train/
+mv train*.triple ../data/benchmark/preprocessed/train/
